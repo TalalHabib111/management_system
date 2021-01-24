d771c6a9828cd8d09cd0eea71dfb414a14489aec
TalalHabib111 <talalhabib1111@github.com>
2021-01-24 17:44:41 +0500

    signup

--- app/src/main/java/com/example/employeemanagement/signup.java
+++ app/src/main/java/com/example/employeemanagement/signup.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Login Form");
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("SignUp Form");
     }
 }
